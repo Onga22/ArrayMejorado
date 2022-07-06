59960aa706dd250c1fbbe89f52569a4cb5f65b8b
Onga22 <107556703+Onga22@users.noreply.github.com>
2022-07-06 10:57:30 -0300

    Add files via upload

--- ArrayLetra2.java
+++ ArrayLetra2.java
@@ -17,41 +17,40 @@ public class ArrayLetra2 {
         int cont = 0;
         char[] arrayCadena = cadena.toCharArray();
         ArrayList<Integer> arrayPosiciones = new ArrayList();
-        int valorAscii=0;           
         int contDos=0;
         int contUno=1;
+        boolean imp = true;
         
         for (int i = 0; i < letraUno.length(); i++) {
             contDos++;
             if(contDos>contUno){
+            imp=false;
                 System.out.println("Error, ingreso mas de un caracter. Por favor vuelva a intentarlo!! ");
                 break;
             }
-            
-        }
+         }
        
         for (int i = 0; i < arrayCadena.length; i++) {
-            valorAscii= (int) letra;
+             int valorAscii= (int) letra;
            
              if(valorAscii<96||valorAscii>123){
+                 imp=false;
             System.out.println("Error, ingreso un valor numerico, un carecter en  mayuscula o  algun signo. Por favor vuelva a intentarlo!! ");
             break;
              }
             if (arrayCadena[i]==letra){
                  cont++;
                  arrayPosiciones.add(i);
-                 
-                  
-                  }
-             
-             
-        }
+                           }
+              }
+        
+            if(imp==true){
                System.out.println("La letra " + letra + " se repite " + cont + " veces y se encuentra en la posicion " + arrayPosiciones.toString());
         
                 }
     
         }
-        
+}
     
 
 
